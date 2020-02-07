fbcfc744352439e3762e5332ead6d94ad9ad70b3
github Kokusho-gif <asa980589@gmail.com>
2020-02-06 17:02:16 -0800

    初めてのアップロード
    first.pyがメインスクリプト

---
+++ parameters.py
@@ -0,0 +1,10 @@
+# search query
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"'
+
+
+# file were scraped profile information will be stored
+file_name = 'results_file.xlsx'
+
+# login credentials
+linkedin_username = 'asa980589@gmail.com'
+linkedin_password = '3131521590'
